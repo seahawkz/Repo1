ba70ae3c53b2c5af446cd273b2cd7b4c53095bc5
Derek <derekanderson8@msn.com>
2021-02-09 17:17:32 -0800

    Updated Python install script

--- Powershell/Python.ps1
+++ Powershell/Python.ps1
@@ -2,7 +2,18 @@
 $wshell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
 Start-Process -FilePath .\python-3.9.1-amd64.exe -ArgumentList '/quiet','InstallAllUsers=1','PrependPath=1' -Wait
 $wshell.Popup("Python 3.9.1 should now be installed",60,"Python is being installed now",48+1)
+## update pip
+python -m pip install --upgrade pip
+## intall netmiko
+pip install netmiko
+## install pyserial
+pip install pyserial
+## install urllib3
+pip install urllib3
+##
 
+Start-Process -FilePath .\Git-2.30.0.2-64-bit.exe -ArgumentList '/VERYSILENT' -Wait
 
+$wshell.Popup("Git 2.30 should now be installed",60,"Git is being installed now",48+1)
 
 
