c11a9799cf0369bac59a089e8f9b1cc240811d60
Derek <derekanderson8@msn.com>
2021-03-11 15:59:57 -0800

    Updated Python

--- Powershell/Python.ps1
+++ Powershell/Python.ps1
@@ -8,7 +8,7 @@ $wshell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
 Start-Process -FilePath .\python-3.9.1-amd64.exe -ArgumentList '/quiet','InstallAllUsers=1','PrependPath=1' -Wait
 $wshell.Popup("Python 3.9.1 should now be installed",60,"Python is being installed now",48+1)
 ## Install Git
-Start-Process -FilePath .\Git-2.30.0.2-64-bit.exe -ArgumentList '/VERYSILENT' -Wait
+Start-Process -FilePath .\Git_Installer.exe -ArgumentList '/VERYSILENT' -Wait
 
 $wshell.Popup("Git 2.30 should now be installed",60,"Git is being installed now",48+1)
 
